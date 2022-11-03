afd1663712a251c59eda3f5423c62954640e46e7
Lukas Bestle <mail@lukasbestle.com>
2022-11-03 10:21:16 +0100

    Initial commit

---
+++ Paddle.lua
@@ -0,0 +1,400 @@
+-- MoneyMoney extension for Paddle vendor accounts
+-- https://github.com/lukasbestle/moneymoney-paddle
+--
+---------------------------------------------------------
+--
+-- MIT License
+--
+-- Copyright (c) 2022 Lukas Bestle
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
+WebBanking{
+  version     = 1.00,
+  url         = "https://vendors.paddle.com/",
+  services    = {"Paddle"},
+  description = string.format(
+    MM.localizeText("Get balance and transactions for %s"),
+    "Paddle"
+  )
+}
+
+local connection = Connection()
+
+-- define local functions
+local groupTransactions, parseAmount, parseDate, startTime, tableSum
+
+-----------------------------------------------------------
+
+function SupportsBank (protocol, bankCode)
+  return protocol == ProtocolWebBanking and bankCode == "Paddle"
+end
+
+function InitializeSession (protocol, bankCode, username, reserved, password)
+  -- first authenticate to the API, which sets a cookie
+  local requestBody = JSON():set{email=username, password=password}:json()
+  local responseBody = connection:request(
+    "POST",
+    "https://api.paddle.com/login",
+    requestBody,
+    "application/json",
+    {Accept="application/json"}
+  )
+
+  -- check for auth errors
+  local responseData = JSON(responseBody):dictionary()
+  if responseData.error then
+    local errorData = responseData.error
+
+    -- invalid credentials
+    if errorData.code == "forbidden.invalid_credentials" then
+      return LoginFailed
+    end
+
+    -- other error, return full error
+    return string.format(
+      MM.localizeText("The web server %s responded with the error message:\n»%s«\nPlease try again later."),
+      "api.paddle.com",
+      errorData.status .. ": " .. errorData.code
+    )
+  end
+
+  -- first authentication step (API) has succeeded;
+  -- try to request the UI, which redirects a bunch,
+  -- does OAuth magic and sets even more cookies
+  local html = HTML(connection:get(url))
+
+  -- ensure that we are indeed logged in
+  if html:xpath("//*[@class='sb-header__vendor-name']"):length() < 1 then
+    -- credentials were correct, but the login still failed for some reason
+    return MM.localizeText("The server responded with an internal error. Please try again later.")
+  end
+
+  -- no error, success
+  return nil
+end
+
+function ListAccounts (knownAccounts)
+  -- request a month worth of transactions to ensure that there
+  -- will be at least one even for smaller accounts;
+  -- we only need the transaction for the account currency
+  local startDate = os.date("%F", os.time() - 60 * 60 * 24 * 30)
+  local endDate = os.date("%F")
+
+  local response = connection:get(url .. "report/balance?start_date=" .. startDate .. "&end_date=" .. endDate .. "&action=view")
+  local html     = HTML(response)
+
+  return {
+    {
+      name = "Paddle",
+      owner = html:xpath("//*[@class='sb-header__vendor-name'][1]"):text(),
+      accountNumber = string.match(response, 'PaddleVars%.vendor = {"id":(%d+)'),
+      portfolio = false,
+      currency = html:xpath("//*[@id='vendor-main']//tbody/tr[1]/td[6]"):text(),
+      type = AccountTypeOther
+    }
+  }
+end
+
+function RefreshAccount (account, since)
+  -- only return transactions if the mode was configured
+  if not account.attributes.GroupTransactions then
+    -- error message for the UI
+    return "The required user-defined field GroupTransactions was not configured, please refer to the README of the Paddle extension."
+  end
+
+  -- request the list of payouts if pending payouts are enabled
+  local payoutsHtml
+  if (account.attributes.PendingPayouts or "true") == "true" then
+    payoutsHtml = HTML(connection:get(url .. "payouts/sent"))
+  end
+
+  -- prepare the request for the first page of the balance report
+  local startDate = os.date("%F", startTime(account, since))
+  local endDate = os.date("%F")
+  local url = url .. "report/balance?start_date=" .. startDate .. "&end_date=" .. endDate .. "&narrative=on&action=view"
+
+  local balance = 0
+  local html
+  local pendingBalance = 0
+  local transactions = {}
+
+  -- follow the pagination links until there is none
+  repeat
+    html = HTML(connection:get(url))
+
+    -- parse the raw HTML table into a transaction array
+    html:xpath("//*[@id='vendor-main']/table/tbody/tr"):each(
+      function (_, element)
+        local children = element:children()
+
+        -- either get the positive credit amount or the
+        -- negative debit amount
+        local amount = parseAmount(children:get(5):text())
+        if amount == 0 then
+          amount = -parseAmount(children:get(4):text())
+        end
+
+        local transaction = {
+          name = children:get(9):text() .. " " .. children:get(8):text(),
+          amount = amount,
+          currency = children:get(7):text(),
+          bookingDate = parseDate(children:get(10):text()),
+          purpose = children:get(3):text(),
+          bookingText = children:get(2):text(),
+          endToEndReference = children:get(1):text()
+        }
+
+        -- separate booking text for refund requests
+        if transaction.purpose == "Refund Request Received" then
+          transaction.bookingText = "REFUND_REQUEST"
+        end
+
+        -- handle pending payouts if configured
+        if transaction.bookingText == "PAYOUT" and (account.attributes.PendingPayouts or "true") == "true" then
+          -- try to find a sent payout by the payout reference
+          local sentPayout = payoutsHtml:xpath("//*[@id='vendor-main']//strong[text()='" .. children:get(8):text() .. "']/ancestor::pui-tr")
+
+          if sentPayout:length() >= 1 then
+            -- payout is sent, set the payment date to make the history graph work correctly
+            transaction.bookingDate = parseDate(sentPayout:children():get(2):text())
+          else
+            -- payout is pending
+            transaction.booked = false
+
+            -- fake the balance to account for the pending payout
+            -- (simulate that the payout is contained in the balance);
+            -- operators are swapped because the amount is negative
+            balance = balance - transaction.amount
+            pendingBalance = pendingBalance + transaction.amount
+          end
+        end
+
+        table.insert(transactions, transaction)
+      end
+    )
+
+    -- get the next URL or unset the URL if there is no next link
+    local nextLink = html:xpath("//*[@id='vendor-main']//*[@class='pagination']//a[@rel='next']")
+    url = nextLink:length() >= 1 and nextLink:attr("href") or ""
+  until not url or url == ""
+
+  -- sort the transactions by booking date descending
+  table.sort(
+    transactions,
+    function (one, two)
+      return one.bookingDate > two.bookingDate
+    end
+  )
+
+  -- group transactions of the same day if configured
+  local groupedTransactions = transactions
+  if account.attributes.GroupTransactions == "true" then
+    -- reset the array and build it from scratch with groups
+    groupedTransactions = {}
+
+    -- the supported grouping types with human-readable label
+    -- (every transaction with different booking text is kept ungrouped)
+    local types = {
+      INVOICING = "invoicing payment",
+      ORDER = "order",
+      ORDER_UNDO_CHECKOUT = "checkout reversal",
+      REFUND = "refund",
+      REFUND_REQUEST = "refund request",
+      REFUND_REVERSAL = "refund reversal",
+      SUBSCRIPTION = "subscription",
+      SUB_PAY_REFUND = "subscription payment refund",
+      SUB_PAY_REFUND_REVERSAL = "subscription payment refund reversal"
+    }
+
+    local currentDate
+    local currentTransactions = {}
+
+    for _, transaction in ipairs(transactions) do
+      local transactionDate = os.date("%F", transaction.bookingDate)
+
+      -- if we have reached another day, group the transactions of the last day
+      if transactionDate ~= currentDate then
+        pendingBalance = pendingBalance + groupTransactions(currentTransactions, types, groupedTransactions)
+
+        -- reset the temporary variables
+        currentDate = transactionDate
+        currentTransactions = {}
+      end
+
+      if types[transaction.bookingText] then
+        -- groupable transaction
+
+        -- initialize table if needed
+        if not currentTransactions[transaction.bookingText] then
+          currentTransactions[transaction.bookingText] = {}
+        end
+
+        table.insert(currentTransactions[transaction.bookingText], transaction)
+      else
+        -- ungroupable transaction, add directly
+        table.insert(groupedTransactions, transaction)
+      end
+    end
+
+    -- create final groups for the last day in the transaction list
+    pendingBalance = pendingBalance + groupTransactions(currentTransactions, types, groupedTransactions)
+  end
+
+  -- add the actual reported balance to the fake payout balance offset
+  balance = balance + parseAmount(html:xpath("//*[@class='financial-stats']/*[@class='balance']"):text())
+
+  return {
+    balance = balance,
+    pendingBalance = pendingBalance,
+    transactions = groupedTransactions
+  }
+end
+
+function FetchStatements (accounts, knownIdentifiers)
+  local startDate = startTime(accounts[1], 0)
+
+  local html = HTML(connection:get(url .. "payouts/sent"))
+
+  -- collect all invoices from all matching payouts
+  local invoices = {}
+  html:xpath("//*[@id='vendor-main']//pui-tbody//pui-tr"):each(
+    function (_, row)
+      local children = row:children()
+
+      local creationDate = parseDate(children:get(2):text())
+      if creationDate < startDate then
+        -- skip payouts before the start date
+        return
+      end
+
+      -- find all attachments of this payout
+      row:xpath(".//pui-button"):each(
+        function (_, button)
+          local href = button:attr("href")
+
+          local invoice = {
+            creationDate = creationDate,
+            name = href:match("(%d+/[^/]+)$"):gsub("/", "_"),
+            identifier = href
+          }
+
+          -- only download the invoice if it wasn't already downloaded
+          if not knownIdentifiers[invoice.identifier] then
+            invoice.pdf, _, _, invoice.filename = connection:get(invoice.identifier)
+            table.insert(invoices, invoice)
+          end
+        end
+      )
+    end
+  )
+
+  return {statements=invoices}
+end
+
+function EndSession ()
+  connection:get(url .. "logout")
+end
+
+-----------------------------------------------------------
+
+-- Reduces each group of transactions into one transaction,
+-- appends it to the target table and returns the total
+-- pending balance of the current day
+function groupTransactions (transactions, types, targetTable)
+  local pendingBalance = 0
+
+  for bookingText, typeTransactions in pairs(transactions) do
+    local groupName = types[bookingText]
+
+    -- append plural s if there are multiple transactions
+    if #typeTransactions > 1 then
+      groupName = groupName .. "s"
+    end
+
+    local amount = tableSum(typeTransactions, "amount")
+    local bookingDate = typeTransactions[1].bookingDate
+    local currency = typeTransactions[1].currency
+
+    -- mark transactions of the current day as pending
+    local booked = os.date("%F", bookingDate) ~= os.date("%F")
+
+    table.insert(targetTable, {
+      name = #typeTransactions .. " " .. groupName,
+      amount = amount,
+      currency = currency,
+      bookingDate = bookingDate,
+      purpose = "Average: " .. string.format("%.2f", amount / #typeTransactions) .. " " .. currency,
+      bookingText = bookingText,
+      booked = booked
+    })
+
+    if booked == false then
+      pendingBalance = pendingBalance + amount
+    end
+  end
+
+  return pendingBalance
+end
+
+-- Extracts a number with optional decimals from a string
+function parseAmount (amount)
+  -- extract the actual amount without currency and
+  -- remove the thousand separators for number parsing
+  amount = amount:match("([0-9.,]+)"):gsub(",", "")
+
+  return tonumber(amount)
+end
+
+-- Extracts an ISO 8601 date (YYYY-MM-DD) from a string
+-- and converts it into a POSIX timestamp
+function parseDate (date)
+  if not date then
+    return nil
+  end
+
+  local datePattern = "(%d%d%d%d)%-(%d%d)%-(%d%d)"
+  local year, month, day = date:match(datePattern)
+  return os.time{year=year, month=month, day=day}
+end
+
+-- Returns the provided POSIX timestamp,
+-- but limited to the minimum configured start date
+-- and at most one year ago
+function startTime (account, timestamp)
+  local oneYearAgo = os.time() - 60 * 60 * 24 * 365
+
+  return math.max(
+    timestamp,
+    parseDate(account.attributes.StartDate) or 0,
+    oneYearAgo
+  )
+end
+
+-- Returns the sum of a specific field of
+-- all elements of a table
+function tableSum (table, field)
+  local sum = 0
+
+  for _, value in pairs(table) do
+    sum = sum + value[field]
+  end
+
+  return sum
+end
---
+++ README.md
@@ -0,0 +1,57 @@
+# Paddle extension for MoneyMoney
+
+This extension for [MoneyMoney](https://moneymoney-app.com/) retrieves the balance and transactions of [Paddle](https://www.paddle.com) vendor accounts and optionally downloads the payout invoices to the MoneyMoney statements view.
+
+**Note:** Login with two-factor authentication (TOTP) is not supported by this extension as it uses web scraping. If Paddle provides API routes for the balance and payouts some day, the extension can be rewritten to use API keys. Until then, if your main admin account uses TOTP, please create a secondary account with the "Finances" permission.
+
+## How to use
+
+### Installation
+
+1. Download the signed extension file of the [latest release](https://github.com/lukasbestle/moneymoney-paddle/releases/latest).
+2. Open the database directory of MoneyMoney as described on the [MoneyMoney website](https://moneymoney-app.com/extensions/).
+3. Place the extension file (`Paddle.lua`) in the `Extensions` directory of your MoneyMoney database.
+4. Add a new account of type "Paddle". You can log in with your credentials for <https://vendors.paddle.com>.
+
+### Setup account refresh
+
+The first account refresh cycle of your newly created account will fail (i.e. MoneyMoney will not display a balance or any transactions in your Paddle account). This is because you first need to set required configuration options.
+
+To set the options, right-click on your Paddle account in the MoneyMoney sidebar, open the account settings and switch to the "Notes" page. There you will find a table for user-defined fields:
+
+<img alt="Screenshot of the options in the MoneyMoney user-defined fields table" src="options.png" width="574">
+
+The `GroupTransactions` option is required, the other options are optional (see below). Please set the options you like, confirm the settings dialog and manually refresh your account.
+
+### Setup invoice downloads
+
+If you want MoneyMoney to automatically download your Paddle invoices from your configured start date, please open the MoneyMoney statements settings and enable the toggle for your Paddle account. MoneyMoney will automatically download your invoices immediately.
+
+## Options
+
+### `GroupTransactions: true | false` (required)
+
+To keep the size of your MoneyMoney database sane, this option allows to create groups of all similar transactions of the same day. Instead of a single transaction per order, subscription purchase and refund, you will see one transaction for all orders, one for all subscription purchases and one for all refunds of the same day, e.g. like so:
+
+> **5 orders**
+> Average: 92.57 EUR
+
+To get this behavior, please set the option to `true`. If you prefer to get the original data from Paddle without grouping, set it to `false`.
+
+### `PendingPayouts: true | false` (defaults to `true`)
+
+Paddle already subtracts a payout from the balance on the first day of the month, but the actual day of the bank transfer may be much later. If you use an account group in MoneyMoney for your bank account and Paddle account, you would see a large gap between the first of the month and the day of the payout bank transfer.
+
+This extension works around this issue by default. This has the following effects:
+
+- Payouts that have not yet been transferred are displayed as a pending transaction in MoneyMoney.
+- While a payout is pending, the displayed account balance is increased by the amount of the payout.
+- The booking day of a transferred payout is set to the day of the transfer.
+
+If you prefer the unmodified data from Paddle, set the option to `false`.
+
+### `StartDate: YYYY-MM-DD` (defaults to one year ago)
+
+If you have a large account history, you may not want to download all past transactions and invoices. With this option, you can set a start date in ISO 8601 format (`YYYY-MM-DD`).
+
+**Note:** A start date beyond one year ago is not supported. This is because Paddle only displays the latest 15 payouts on the first page of the payouts view, so the logic for pending payouts would fail on older payouts. If you want to see older transactions or invoices in MoneyMoney, please import them manually.
